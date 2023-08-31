a0a1c7cd4c4f45ccea82bbdd0699fa1c6a363b68
Oliver Tacke <o.tacke@posteo.de>
2023-08-31 20:30:31 +0200

    Re-add selecting chapter on startup

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -379,6 +379,15 @@ export default class Portfolio extends H5P.EventDispatcher {
 
     this.updateFooter();
 
+    // Set start view.
+    const currentChapter = this.chapters.get(this.currentChapterId);
+    this.sideBar.handleClicked({
+      hierarchy: currentChapter.getHierarchy(),
+      target: {
+        chapter: currentChapter.getSubContentId()
+      }
+    });
+
     setTimeout(() => {
       this.trigger('attached');
     }, 0);
