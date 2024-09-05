8dc812559ad54026fb077eb41800fdbefc582614
Oliver Tacke <o.tacke@posteo.de>
2024-09-05 15:18:27 +0200

    Fix task detection

--- src/scripts/services/util.js
+++ src/scripts/services/util.js
@@ -100,7 +100,7 @@ export default class Util {
       return false;
     }
 
-    if (instance.isTask) {
+    if (typeof instance.isTask === 'boolean') {
       return instance.isTask; // Content will determine if it's task on its own
     }
 
